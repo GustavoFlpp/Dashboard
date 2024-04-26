92099f07b0a2c96e5215e147d80f792309cfe22a
Gustavo Felippe Barbosa <113379320+GustavoFlpp@users.noreply.github.com>
2024-04-26 13:59:44 -0300

    Adicionando o CSS

--- dashboards.py
+++ dashboards.py
@@ -2,8 +2,18 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+
 # Define a configuração da página como "wide"
 st.set_page_config(layout="wide")
+
+# Abre o arquivo "styles.css" em modo de leitura e o associa à variável 'f'
+with open("styles.css") as f:
+    # Lê o conteúdo do arquivo CSS e o armazena em uma string
+    css_content = f.read()
+    # Utiliza o componente de markdown do Streamlit para inserir o conteúdo CSS diretamente no aplicativo da web
+    # O conteúdo é inserido entre tags <style> para aplicação correta do CSS
+    st.markdown(f"<style>{css_content}</style>", unsafe_allow_html=True)
+
 
 # Lê o arquivo CSV e cria um DataFrame
 df = pd.read_csv("supermarket_sales.csv", sep=";", decimal=",")
@@ -31,33 +41,31 @@ col1, col2 = st.columns(2)
 # Cria três colunas na interface do Streamlit
 col3, col4, col5 = st.columns(3)
 
-# O use_container_width=True faz com o gráfico se adapte com a largura do navegador
-
 # Cria um gráfico de barras do faturamento por dia, dividido por cidade
-fig_date = px.bar(df_filtered, x="Date", y="Total",color="City", title="Faturamento por dia")
+fig_date = px.bar(df_filtered, x="Date", y="Total",color="City", title="Daily billing")
 # Exibe o gráfico na primeira coluna
 col1.plotly_chart(fig_date, use_container_width=True)
 
 # Cria um gráfico de barras do faturamento por tipo de produto, dividido por cidade
-fig_prod = px.bar(df_filtered, x="Date", y="Product line",color="City", title="Faturamento por tipo de produto", orientation="h")
+fig_prod = px.bar(df_filtered, x="Date", y="Product line",color="City", title="Billing by product type", orientation="h")
 # Exibe o gráfico na segunda coluna
 col2.plotly_chart(fig_prod, use_container_width=True)
 
 # Agrupa o DataFrame filtrado por cidade e soma o total de vendas
 city_total = df_filtered.groupby("City")[["Total"]].sum().reset_index()
 # Cria um gráfico de barras do faturamento por filial
-fig_city = px.bar(city_total, x="City", y="Total", title="Faturamento por filial")
+fig_city = px.bar(city_total, x="City", y="Total",color="City", title="Billing by branch")
 # Exibe o gráfico na terceira coluna
 col3.plotly_chart(fig_city, use_container_width=True)
 
 # Cria um gráfico de pizza do faturamento por tipo de pagamento
-fig_kind = px.pie(df_filtered, values="Total", names="Payment", title="Faturamento por tipo de pagamento")
+fig_kind = px.pie(df_filtered, values="Total", names="Payment", title="Billing by payment")
 # Exibe o gráfico na quarta coluna
 col4.plotly_chart(fig_kind, use_container_width=True)
 
 # Agrupa o DataFrame filtrado por cidade e calcula a média das avaliações
 city_total = df_filtered.groupby("City")[["Rating"]].mean().reset_index()
 # Cria um gráfico de barras da avaliação por cidade
-fig_rating = px.bar(df_filtered, x="City", y="Rating", title="Avaliação")
+fig_rating = px.bar(df_filtered, x="City", y="Rating", color="City", title="Evaluation")
 # Exibe o gráfico na quinta coluna
 col5.plotly_chart(fig_rating, use_container_width=True)
